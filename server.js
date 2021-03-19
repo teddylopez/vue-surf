6ba0ba2be139dedff1216443aa75fdd71129208e
tedlopez <tedmlopez@gmail.com>
2021-03-19 14:59:36 -0400

    Fix error when page is refreshed by specifying the base url

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require("express");
 var path = require("path");
 var serveStatic = require("serve-static");
 const cors = require("cors");
-
 const app = express();
-app.use(serveStatic(__dirname + "/dist"));
+// app.use(serveStatic(__dirname + "/dist"));
+const buildLocation = "dist";
+app.use(express.static(`${buildLocation}`));
+app.use((req, res, next) => {
+  if (!req.originalUrl.includes(buildLocation)) {
+    res.sendFile(`${__dirname}/${buildLocation}/index.html`);
+  } else {
+    next();
+  }
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log("server started " + port);
